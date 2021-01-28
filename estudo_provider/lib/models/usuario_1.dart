f7ebb2eb03352e89162e05dd76d1573590be60c6
Bruno <bmb_bezerra@outlook.com>
2021-01-28 17:20:18 -0300

    App criado.

---
+++ estudo_provider/lib/models/usuario_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Usuario1 extends ChangeNotifier {
+  String nome = 'Null';
+  String sobrenome = 'Null';
+  int idade = 0;
+
+  void addUsuario(String novoNome, String novoSobrenome, int novaIdade) {
+    nome = novoNome;
+    sobrenome = novoSobrenome;
+    idade = novaIdade;
+
+    notifyListeners();
+  }
+}
